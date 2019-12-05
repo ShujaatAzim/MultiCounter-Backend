94e177c6f91661499a3f49811a7545ac83e9872b
Shujaat Azim <shujaat.azim@gmail.com>
2019-12-04 19:00:54 -0500

    made many changes in an effort to properly render the associations in json, but to no avail. also added a lot more seed data

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,6 +1,5 @@
 class Counter < ApplicationRecord
-
-  has_many :counter_tags
-  has_many :tags, :through => :counter_tags
+  
+  has_many :tags, through: :counter_tags
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,5 @@
 class Tag < ApplicationRecord
 
-  has_many :counter_tags
-  has_many :counters, :through => :counter_tags
+  has_many :counters, through: :counter_tags
 
 end
