174f67518c7b2cafcab313d996ca891c9eb16703
Shujaat Azim <shujaat.azim@gmail.com>
2019-12-22 23:54:10 -0500

    methods also to counters and tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,6 +9,20 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
     render json: TagSerializer.new(@tag.includes(:counters)).to_serialized_json
   end
-  
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 
 end
